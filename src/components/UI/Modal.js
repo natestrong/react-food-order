715a756745ac74330dc620acfbea0d58f102ea98
nstrong <nathanstrong.fx@gmail.com>
2021-09-11 13:00:02 -0700

    init

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,5 +1,19 @@
 import classes from './Modal.module.css';
+import ReactDOM from 'react-dom';
+
+function Backdrop(props) {
+    return <div className={classes.backdrop}></div>;
+}
+
+function ModalOverlay(props) {
+    return <div className={classes.modal}>
+        <div className={classes.content}>{props.children}</div>
+    </div>;
+}
 
 export default function Modal(props) {
-
+    return (<>
+        {ReactDOM.createPortal(<Backdrop/>)}
+        {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>)}
+    </>);
 }
